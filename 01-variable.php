e516d9386cfe13bf93b6806d3b3fd130217f70f1
EliasDFranco <eliasfranco20160055@gmail.com>
2025-01-16 15:28:17 -0300

    Agregando nuevas pruebas de echo

--- 01-variable.php
+++ 01-variable.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
 echo "Hola Mundo";
+echo "<br>";
 $nombre = "Elias";
-echo $nombre
+echo $nombre;
+echo "<h2> PHP is very FUN!! </h2>";
+echo "<h3>I living in Japan</h3>";
+
+$bookOne = "The Lord of the rings the fellowship of the ring";
+$bookTwo = "Harry Potter and the Prisoner of Azkaban";
+
+echo $bookOne . " and " . $bookTwo;
+
 ?>
+    </body>
+</html>
