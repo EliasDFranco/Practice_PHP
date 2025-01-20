56296a50485f161b1df5cf80a95c46dda132a549
EliasDFranco <eliasfranco20160055@gmail.com>
2025-01-20 09:30:44 -0300

    Subiendo los ciclos while, do while y for en php

---
+++ 08-Ciclos.php
@@ -0,0 +1,24 @@
+<?php 
+// Ciclo while
+$num = 1;
+while ($num <= 10) {
+    echo $num;
+    $num += 1;
+}
+echo "<br>";
+
+// Ciclo do while
+$num2 = 10;
+do {
+    echo $num2;
+    $num2++;
+} while ($num2 <= 15);
+echo "<br>";
+
+// Ciclo for
+$num3 = 20;
+for ($num3 = 4; $num3 <= 10 ; $num3++) { 
+    echo $num3 ;
+}
+echo "<br>";
+?> 
